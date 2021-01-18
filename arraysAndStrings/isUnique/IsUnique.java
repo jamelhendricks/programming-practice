2f046346ad307ef45dcb3354b75ab6477bef1bb1
Jamel Hendricks <jamelmhendricks@gmail.com>
2021-01-17 21:07:42 -0500

    add isUnique naive solution

--- arraysAndStrings/isUnique/IsUnique.java
+++ arraysAndStrings/isUnique/IsUnique.java
@@ -1,6 +1,32 @@
+import java.util.HashMap;
+
 public class IsUnique {
+    // determine if a string has all unique characters
+
+    public static boolean isUnique(String input){
+        boolean unique = true;
+
+        HashMap<Character, Integer> strMap = new HashMap<Character, Integer>();
+
+        int strLength = input.length();
+        for (int i = 0; i < strLength; i++){
+            char c = input.charAt(i);
+
+            if(strMap.keySet().contains(c)){
+                return false;
+            } else {
+                strMap.put(c, 1);
+            }
+        }
+
+        return unique;
+    }
+
 
     public static void main(String[] args) {
+        System.out.println("unique: " + isUnique("unique"));
+        System.out.println("foo: " + isUnique("foo"));
+        System.out.println("bar: " + isUnique("bar"));
         
     }
 }
